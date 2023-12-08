925a4ae65ec14879a7f10a16e7c383a72344829f
Jurriaan Pruijs <jpruijs@bol.com>
2023-12-08 09:42:06 +0100

    Day 1 - part 1

---
+++ day08/src/main.rs
@@ -0,0 +1,98 @@
+use std::collections::HashMap;
+
+use crate::Move::{LEFT, RIGHT};
+
+const INPUT: &str = include_str!("input.txt");
+const EXAMPLE_1: &str = include_str!("example-1.txt");
+const EXAMPLE_2: &str = include_str!("example-2.txt");
+
+fn main() {
+    print_answer("one (example 1)", &one(EXAMPLE_1), "2");
+    print_answer("one (example 2)", &one(EXAMPLE_2), "6");
+    print_answer("one", &one(INPUT), "");
+    // print_answer("two (example)", &two(EXAMPLE_1), "");
+    // print_answer("two", &two(INPUT), "");
+}
+
+fn print_answer(name: &str, actual: &str, expected: &str) {
+    match actual == expected {
+        true => println!("{name}: {actual} (OK)"),
+        false => println!("{name}: {actual} (ERROR: expected {expected})"),
+    }
+}
+
+fn one(input: &str) -> String {
+    let world = parse(input);
+    let moves = world.moves.clone();
+
+    let mut steps = 0usize;
+    let mut current = String::from("AAA");
+
+    while current != "ZZZ" {
+        let next_move_index = steps % moves.len();
+        let next_move = moves.get(next_move_index).expect("expect node to exist");
+        current = world.go(current, next_move);
+        steps += 1;
+    }
+
+    steps.to_string()
+}
+
+fn two(input: &str) -> String {
+    String::new()
+}
+
+fn parse(input: &str) -> World {
+    let (moves, nodes) = input.split_once("\n\n").expect("double newline");
+
+    World {
+        moves: moves
+            .chars()
+            .map(|c| match c {
+                'L' => LEFT,
+                'R' => RIGHT,
+                _ => panic!("expect L or R"),
+            })
+            .collect(),
+        nodes: nodes
+            .lines()
+            .map(|l| {
+                let name = String::from(&l[0..3]);
+                let left = String::from(&l[7..10]);
+                let right = String::from(&l[12..15]);
+
+                (name.clone(), Node { name, left, right })
+            })
+            .collect(),
+    }
+}
+
+#[derive(Debug, Clone)]
+struct World {
+    moves: Vec<Move>,
+    nodes: HashMap<String, Node>,
+}
+
+impl World {
+    fn go(&self, from: String, to: &Move) -> String {
+        let destination = self.nodes.get(&*from).expect("expect from");
+
+        match to {
+            LEFT => destination.left.clone(),
+            RIGHT => destination.right.clone(),
+        }
+    }
+}
+
+#[derive(Debug, Copy, Clone)]
+enum Move {
+    LEFT,
+    RIGHT,
+}
+
+#[derive(Debug, Clone)]
+struct Node {
+    name: String,
+    left: String,
+    right: String,
+}
