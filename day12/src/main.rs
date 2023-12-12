c200c3b4252d7edb30a22e68e8a9616fc552cd4b
Jurriaan Pruijs <jpruijs@bol.com>
2023-12-12 20:43:07 +0100

    Day 12 - part 2

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::fmt::{Display, Formatter, Write};
 use std::ops::BitXor;
 
@@ -6,10 +6,10 @@ const INPUT: &str = include_str!("input.txt");
 const EXAMPLE: &str = include_str!("example.txt");
 
 fn main() {
-    // print_answer("one (example)", &one(EXAMPLE), "21");
-    print_answer("one", &one(INPUT), "");
-    // print_answer("two (example)", &two(EXAMPLE), "");
-    // print_answer("two (example)", &two(INPUT), "");
+    print_answer("one (example)", &one(EXAMPLE), "21");
+    print_answer("one", &one(INPUT), "7344");
+    print_answer("two (example)", &two(EXAMPLE), "525152");
+    print_answer("two (example)", &two(INPUT), "");
 }
 
 fn print_answer(name: &str, actual: &str, expected: &str) {
@@ -20,46 +20,47 @@ fn print_answer(name: &str, actual: &str, expected: &str) {
 }
 
 fn one(input: &str) -> String {
-    let world = World::parse(input);
+    let world = World::parse(input, 1);
 
-    // println!("{world}");
-    //
-    // world.records.iter().for_each(|r| {
-    //     let extra_length = r.extra_length();
-    //     println!("{r} {extra_length}");
-    //
-    //     r.candidates().iter().for_each(|c| {
-    //         println!("{:#b}", c);
-    //     })
-    // });
-    //
-    // let max_extra_length_record = world
-    //     .records
-    //     .iter()
-    //     .max_by_key(|r| r.extra_length())
-    //     .expect("at least one");
-    // let max_extra_length = max_extra_length_record.extra_length();
-    //
-    // println!("max: {max_extra_length}");
-    //
-    // let combinations: u32 = world
-    //     .records
-    //     .iter()
-    //     .map(|r| 2u32.pow(r.extra_length()))
-    //     .sum();
-    //
-    // println!("total: {combinations}");
+    world.records.iter().for_each(|record| {
+        // let record = world.records.iter().nth(0).expect("it");
+        let mut cache: HashMap<CacheKey, u64> = HashMap::new();
+        let candidates = record.candidates(0, 0, 0, &mut cache);
+
+        println!("{record} {candidates}");
+    });
 
     world
         .records
         .iter()
-        .map(|r| r.candidates().iter().len() as u32)
-        .sum::<u32>()
+        .map(|record| {
+            let mut cache: HashMap<CacheKey, u64> = HashMap::new();
+            record.candidates(0, 0, 0, &mut cache)
+        })
+        .sum::<u64>()
         .to_string()
 }
 
 fn two(input: &str) -> String {
-    String::new()
+    let world = World::parse(input, 5);
+
+    world.records.iter().for_each(|record| {
+        let mut cache: HashMap<CacheKey, u64> = HashMap::new();
+        // let record = world.records.iter().nth(0).expect("it");
+        let candidates = record.candidates(0, 0, 0, &mut cache);
+
+        println!("{record} {candidates}");
+    });
+
+    world
+        .records
+        .iter()
+        .map(|record| {
+            let mut cache: HashMap<CacheKey, u64> = HashMap::new();
+            record.candidates(0, 0, 0, &mut cache)
+        })
+        .sum::<u64>()
+        .to_string()
 }
 
 struct World {
@@ -67,9 +68,9 @@ struct World {
 }
 
 impl World {
-    fn parse(input: &str) -> Self {
+    fn parse(input: &str, copies: u32) -> Self {
         Self {
-            records: input.lines().map(Record::parse).collect(),
+            records: input.lines().map(|l| Record::parse(l, copies)).collect(),
         }
     }
 }
@@ -87,20 +88,26 @@ impl Display for World {
 
 struct Record {
     length: u32,
-    has_spring: u32,
-    has_no_spring: u32,
+    has_spring: u128,
+    has_no_spring: u128,
     groups: Vec<u32>,
 }
 
 impl Record {
-    fn parse(input: &str) -> Self {
+    fn parse(input: &str, copies: u32) -> Self {
         let (springs, groups) = input.split_once(' ').expect("space");
 
+        let springs = (0..copies)
+            .map(|_| springs)
+            .collect::<Vec<&str>>()
+            .join("?");
+        let groups = (0..copies).map(|_| groups).collect::<Vec<&str>>().join(",");
+
         let length = springs.len() as u32;
-        let mut has_spring = 0u32;
-        let mut has_no_spring = 0u32;
+        let mut has_spring = 0u128;
+        let mut has_no_spring = 0u128;
         (0..length).for_each(|i| {
-            let bit = 2u32.pow(length - i - 1);
+            let bit = 2u128.pow(length - i - 1);
             match springs.chars().nth(i as usize).expect("char at index") {
                 '#' => has_spring |= bit,
                 '.' => has_no_spring |= bit,
@@ -122,92 +129,98 @@ impl Record {
         }
     }
 
-    fn extra_length(&self) -> u32 {
-        let length_of_all_groups = self.groups.iter().sum::<u32>();
-        let minimum_space_between_groups = self.groups.len() as u32 - 1;
-
-        self.length - length_of_all_groups - minimum_space_between_groups
+    fn space(&self) -> u32 {
+        self.length - self.groups.iter().sum::<u32>() + 1 - self.groups.len() as u32
     }
 
-    fn candidates(&self) -> HashSet<u32> {
-        let space: u32 = self.length - self.groups.iter().sum::<u32>();
-        let number_of_slots = self.groups.len() as u32 + 1;
+    fn candidates(
+        &self,
+        acc: u128,
+        from: u32,
+        group_index: usize,
+        cache: &mut HashMap<CacheKey, u64>,
+    ) -> u64 {
+        let group_size = self.groups[group_index];
 
-        let slots = self.slots(space, number_of_slots, true);
+        let mut result = 0;
 
-        slots
-            .iter()
-            .map(|slot| {
-                // println!("{:?}", slot);
+        let mut passed_spring = false;
+        let mut from = from;
 
-                self.groups
-                    .iter()
-                    .enumerate()
-                    .fold(0u32, |acc, (index, group)| {
-                        let space: u32 = (0..=index).map(|i| slot[i]).sum();
-                        let previous_groups: u32 = (0..index).map(|i| self.groups[i]).sum();
-                        let start = space + previous_groups;
-                        let end = start + group - 1;
+        while !passed_spring && self.length >= from && (self.length - from) >= group_size {
+            let mask = (2u128.pow(group_size) - 1) << (self.length - group_size - from);
+            let cancidate = acc | mask;
 
-                        // println!("{index} {space} {group} {previous_groups} {start} {end}");
+            let final_group = group_index == self.groups.len() - 1;
+            let has_remaining_positions = self.length - from - group_size > 0;
+            let no_negative_springs = self.has_no_spring & mask == 0;
+            let possible_no_spring_after = !has_remaining_positions
+                || 1u128 << (self.length - group_size - from - 1) & self.has_spring == 0;
+            let possible_no_tail = !final_group
+                || !has_remaining_positions
+                || (2u128.pow(self.length - from - group_size) - 1) & self.has_spring == 0;
 
-                        (start..=end).fold(acc, |acc, bit_index| {
-                            // println!("  {bit_index}");
-                            let bit_mask = 2u32.pow(self.length - bit_index - 1);
-                            // println!("  {:#b}", bit_mask);
+            if final_group {
+                // println!("check: {:032b} {no_negative_springs} {possible_no_spring_after} {possible_no_tail}", cancidate);
+            }
+            // println!("{:07b}", mask);
+            // println!("{:07b}", self.has_spring);
+            // println!("{:07b}", self.has_no_spring);
+            // println!(
+            //     "{:07b} {no_negative_springs} {possible_no_spring_after} {possible_no_tail} {final_group} {group_index}",
+            //     cancidate
+            // );
 
-                            acc | bit_mask
-                        })
-                    })
-            })
-            .filter(|c| {
-                let result = self.has_spring & c == self.has_spring;
+            if no_negative_springs && possible_no_spring_after && possible_no_tail {
+                result += match final_group {
+                    true => {
+                        let ok_springs = cancidate | self.has_spring == self.has_spring;
+                        let ok_no_springs = !cancidate | self.has_no_spring == self.has_no_spring;
 
-                // println!("  {:0>32b}", self.has_spring);
-                // println!("  {:0>32b}", c);
-                // println!("  {:0>32b}", self.has_spring & c);
-                // println!("-- ({})", result);
+                        // println!("found: {:032b} {ok_springs} {ok_no_springs}", cancidate);
 
-                result
-            })
-            .filter(|c| {
-                let result = self.has_no_spring | !c == !c;
+                        1u64
+                    }
+                    false => {
+                        let new_acc = acc | mask;
+                        let new_from = from + group_size + 1;
+                        let new_group_index = group_index + 1;
 
-                // println!("  {:0>32b}", self.has_no_spring);
-                // println!("  {:0>32b}", !c);
-                // println!("  {:0>32b}", self.has_no_spring | !c);
-                // println!(">> ({})", result);
+                        let cache_key = CacheKey {
+                            from: new_from,
+                            group_index: new_group_index,
+                        };
 
-                result
-            })
-            .collect()
-    }
+                        let result = cache.get(&cache_key);
 
-    fn slots(&self, space: u32, slots: u32, first: bool) -> Vec<Vec<u32>> {
-        let start = match first {
-            true => 0,
-            false => 1,
-        };
+                        match result {
+                            None => {
+                                let value =
+                                    self.candidates(new_acc, new_from, new_group_index, cache);
 
-        match slots {
-            1 => (0..=space).map(|l| vec![l]).collect::<Vec<Vec<u32>>>(),
-            _ => (start..=space)
-                .flat_map(|l| {
-                    let mut tails = self.slots(space - l, slots - 1, false);
+                                cache.insert(cache_key, value);
 
-                    tails.iter_mut().for_each(|tail| tail.insert(0, l));
+                                value
+                            }
+                            Some(value) => *value,
+                        }
+                    }
+                }
+            }
 
-                    tails
-                })
-                .collect::<Vec<Vec<u32>>>(),
+            from += 1;
+
+            passed_spring = 2u128.pow(self.length - from) & self.has_spring != 0
         }
+
+        result
     }
 }
 
 impl Display for Record {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         (0..self.length).rev().for_each(|i| {
-            let bit = 2u32.pow(i);
+            let bit = 2u128.pow(i);
             let has_spring = self.has_spring & bit > 0;
             let has_no_spring = self.has_no_spring & bit > 0;
             match (has_spring, has_no_spring) {
@@ -230,4 +243,10 @@ impl Display for Record {
 
         Ok(())
     }
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+struct CacheKey {
+    from: u32,
+    group_index: usize,
 }
