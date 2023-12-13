999bef86fb5d8eb430fd0217fd44f9327a596aca
Jurriaan Pruijs <jpruijs@bol.com>
2023-12-13 08:12:07 +0100

    Day 13 - part 2

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -4,10 +4,10 @@ const INPUT: &str = include_str!("input.txt");
 const EXAMPLE: &str = include_str!("example.txt");
 
 fn main() {
-    print_answer("one (example)", &one(EXAMPLE), "405");
-    print_answer("one", &one(INPUT), "32035");
-    // print_answer("two (example)", &two(EXAMPLE), "525152");
-    // print_answer("two", &two(INPUT), "1088006519007");
+    // print_answer("one (example)", &one(EXAMPLE), "405");
+    // print_answer("one", &one(INPUT), "32035");
+    print_answer("two (example)", &two(EXAMPLE), "400");
+    print_answer("two", &two(INPUT), "24841 is too low");
 }
 
 fn print_answer(name: &str, actual: &str, expected: &str) {
@@ -21,13 +21,18 @@ fn one(input: &str) -> String {
     World::parse(input)
         .fields
         .iter()
-        .map(|f| f.summary())
+        .map(|f| f.clean_summary())
         .sum::<u32>()
         .to_string()
 }
 
 fn two(input: &str) -> String {
-    String::new()
+    World::parse(input)
+        .fields
+        .iter()
+        .map(|f| f.smudge_summary())
+        .sum::<u32>()
+        .to_string()
 }
 
 struct World {
@@ -79,14 +84,49 @@ impl Field {
         }
     }
 
-    fn summary(&self) -> u32 {
-        match Field::reflection_at(&self.rows) {
-            None => Field::reflection_at(&self.transposed().rows).expect("a reflection") + 1,
+    fn clean_summary(&self) -> u32 {
+        match Field::clean_reflection_at(&self.rows) {
+            None => Field::clean_reflection_at(&self.transposed().rows).expect("a reflection") + 1,
             Some(index) => (index + 1) * 100,
         }
     }
 
-    fn reflection_at(rows: &Vec<u32>) -> Option<u32> {
+    fn smudge_summary(&self) -> u32 {
+        match Field::smudge_reflection_at(&self.rows) {
+            None => Field::smudge_reflection_at(&self.transposed().rows).expect("a reflection") + 1,
+            Some(index) => (index + 1) * 100,
+        }
+    }
+
+    fn smudge_reflection_at(rows: &Vec<u32>) -> Option<u32> {
+        let len = rows.len();
+        (0usize..len - 1)
+            .find(|i| {
+                let rows_to_check = (i + 1).min(len - i - 1);
+
+                let mut smudge_count = 0;
+
+                let result = (0..rows_to_check).all(|row| {
+                    let one_index = i - row;
+                    let other_index = i + 1 + row;
+
+                    let one = rows[one_index];
+                    let other = rows[other_index];
+
+                    let has_smudge = is_power_of_two(one ^ other);
+                    if has_smudge {
+                        smudge_count += 1;
+                    }
+
+                    (one == other || has_smudge) && smudge_count <= 1
+                });
+
+                result && smudge_count == 1
+            })
+            .map(|i| i as u32)
+    }
+
+    fn clean_reflection_at(rows: &Vec<u32>) -> Option<u32> {
         let len = rows.len();
         (0usize..len - 1)
             .find(|i| {
@@ -143,4 +183,8 @@ impl Display for Field {
 
         Ok(())
     }
+}
+
+fn is_power_of_two(number: u32) -> bool {
+    (number != 0) && ((number & (number - 1)) == 0)
 }
